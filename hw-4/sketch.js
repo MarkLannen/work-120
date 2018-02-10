f6eca6738e24f01a0bf5c7a27e74856ecee53dad
Mark Lannen <mark1.lannen@umontana.edu>
2018-02-10 06:41:57 -0700

    Began setting up js file with foundational functions

---
+++ hw-4/sketch.js
@@ -0,0 +1,14 @@
+// setup function
+function setup() {
+  // create canvas to draw on
+  createCanvas(800, 600);
+}
+
+function draw () {
+
+  // background
+  background(rgb(159, 221, 232));
+
+
+
+}
