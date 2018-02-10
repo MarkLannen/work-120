be7d6f8097ff9d971750769be66aec3b8bd48fbd
Mark Lannen <mark1.lannen@umontana.edu>
2018-02-10 16:39:33 -0700

    Began background draw

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -9,18 +9,37 @@ function draw() {
   // background
   background( "rgb(183, 183, 183)");
 
-  /**************************/
+  /***************************/
   /*main sandbox*/
   /**************************/
   push();
 
-  // set the grid center to (x:0, y:0)
+  // set the grid center (x:0, y:0)
   // to canvas center
   translate(400, 300);
+
+  /*************************/
+  /* background*/
+  /*************************/
+
+  /* mount sentinel */
+  push();
+  strokeWeight(2);
+  stroke(120);
+  line( -40, 125, 250, -100);
+
+
+
+
+  pop();
+
 
   /**************************/
   /*body*/
   /**************************/
+
+
+
   push();
 
   pop(); // BODY END!
