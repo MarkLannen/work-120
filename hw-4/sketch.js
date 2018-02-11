1937262f1029aeb8d4e48640c58876ce76a88ba3
Mark Lannen <mark1.lannen@umontana.edu>
2018-02-10 17:40:09 -0700

    started 'M' code

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -37,6 +37,22 @@ function draw() {
   //down slope
   line(300, -107, 400, -50);
 
+  /* the 'M' */
+  // horizontal lines
+  stroke(255);
+  strokeWeight(4);
+  strokeCap(SQUARE);
+  line(170, 0, 182, 0);
+  line(229, 0, 240, 0);
+  line(220, 50, 240, 50);
+  line(170, 50, 190, 50);
+
+  // vertical lines
+  line(180, 0, 180, 50);
+  line(231, 0, 231, 50);
+
+  //slanted lines
+  line(180, 0, 205, 30);
 
 
 
