0517d80dc41678db99ebd5933d3ac1e70ee4afb9
MarkLannenUM <GitErDone_565>
2018-02-12 10:48:40 -0700

    updated hair

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -238,27 +238,44 @@ function draw() {
   //left part
 
   arc(-200, -95, 120, 80, radians(180), TWO_PI);
+  arc(-200, -95, 115, 75, radians(190), TWO_PI);
   arc(-200, -95, 110, 80, radians(160), TWO_PI);
+  arc(-200, -95, 105, 80, radians(160), TWO_PI);
+  arc(-200, -95, 103, 75, radians(160), TWO_PI);
   arc(-200, -95, 100, 80, radians(170), TWO_PI);
   arc(-200, -95, 90, 70, radians(160), TWO_PI);
   arc(-200, -95, 83, 60, radians(170), TWO_PI);
   arc(-200, -95, 77, 50, radians(170), TWO_PI);
   arc(-200, -95, 75, 47, radians(170), TWO_PI);
+  arc(-200, -95, 65, 40, radians(160), TWO_PI);
   arc(-200, -95, 60, 40, radians(160), TWO_PI);
+  arc(-200, -95, 55, 30, radians(170), TWO_PI);
   arc(-200, -95, 50, 30, radians(170), TWO_PI);
   arc(-200, -95, 45, 23, radians(150), TWO_PI);
+  arc(-200, -95, 40, 23, radians(150), TWO_PI);
+  arc(-200, -95, 35, 20, radians(150), TWO_PI);
+  arc(-200, -95, 30, 17, radians(150), TWO_PI);
+  arc(-200, -95, 25, 12, radians(150), TWO_PI);
+  arc(-200, -95, 20, 10, radians(150), TWO_PI);
 
   //right part
 
+  arc(-100, -95, 75, 70, PI, radians(15));
   arc(-100, -95, 70, 70, PI, radians(15));
+  arc(-95, -95, 75, 65, PI, radians(15));
   arc(-95, -95, 75, 60, PI, radians(15));
   arc(-91, -95, 75, 60, PI, radians(15));
+  arc(-91, -95, 75, 55, PI, radians(15));
   arc(-90, -95, 70, 50, PI, radians(30));
   arc(-89, -95, 60, 40, PI, radians(45));
   arc(-85, -95, 60, 35, PI, radians(45));
   arc(-82, -95, 50, 30, PI, radians(45));
   arc(-78, -95, 50, 25, PI, radians(45));
   arc(-76, -95, 50, 20, PI, radians(45));
+  arc(-76, -95, 35, 20, PI, radians(45));
+  arc(-76, -95, 30, 20, PI, radians(45));
+  arc(-76, -95, 25, 20, PI, radians(45));
+  arc(-76, -95, 18, 18, PI, radians(45));
 
   /* end hair*/
 
