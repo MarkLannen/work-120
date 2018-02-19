e69d92d8be21f5fc3b6faa9321cd0b33de255324
Mark Lannen <mark1.lannen@umontana.edu>
2018-02-18 18:45:50 -0700

    added more hair

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -27,8 +27,9 @@ var goateeX = 579;
 // hair variables
 var hairXstart = 380;
 var hairYstart = 190;
-var hairXEnd = 370;
-var hairYEnd = 175;
+var hairXEnd = 380;
+var hairYEnd = 170;
+
 
 //line(width/3.25, 190, width/3.35, 175);
 
@@ -63,8 +64,28 @@ pop();
 /* hair */
 push();
 stroke('rgb(74, 54, 48)');
-strokeWeight(5);
+strokeWeight(10);
 line(hairXstart, hairYstart, hairXEnd, hairYEnd);
+line(hairXstart + 15, hairYstart - 5, hairXEnd + 15, hairYEnd);
+line(hairXstart + 30, hairYstart - 10, hairXEnd + 30, hairYEnd);
+line(hairXstart + 45, hairYstart - 15, hairXEnd + 45, hairYEnd);
+line(hairXstart + 60, hairYstart - 20, hairXEnd + 60, hairYEnd);
+line(hairXstart + 75, hairYstart - 25, hairXEnd + 75, hairYEnd);
+line(hairXstart + 90, hairYstart - 30, hairXEnd + 90, hairYEnd - 15);
+line(hairXstart + 105, hairYstart - 35, hairXEnd + 105, hairYEnd - 20);
+line(hairXstart + 120, hairYstart - 40, hairXEnd + 120, hairYEnd - 25);
+line(hairXstart + 135, hairYstart - 45, hairXEnd + 135, hairYEnd - 30);
+line(hairXstart + 150, hairYstart - 50, hairXEnd + 150, hairYEnd - 35);
+line(hairXstart + 165, hairYstart - 55, hairXEnd + 165, hairYEnd - 40);
+line(hairXstart + 180, hairYstart - 60, hairXEnd + 180, hairYEnd - 45);
+line(hairXstart + 195, hairYstart - 65, hairXEnd + 195, hairYEnd - 60);
+line(hairXstart + 210, hairYstart - 70, hairXEnd + 210, hairYEnd - 65);
+line(hairXstart + 225, hairYstart - 75, hairXEnd + 225, hairYEnd - 70);
+
+
+// growing hair code
+hairYEnd = hairYEnd - 2;
+
 
 pop();
 
