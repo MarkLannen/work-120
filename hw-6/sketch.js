147375c4b4ce3a544714a7e40d7de68905b73d2d
Mark Lannen <mark1.lannen@umontana.edu>
2018-02-26 08:45:03 -0700

    began coding variables and basic shapes

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,10 +1,27 @@
 // define global variables
+var line1 = {
+    x1: 0,
+    y1: 0,
+    x2: 15,
+    y2: 15
+};
+
+var angle = {
+    angle1: 1,
+    angle2: 1,
+    angle3: 1
+};
 
 
+var color1 = {
+    r: 100,
+    g: 50,
+    b: 150
+};
 
 function setup() {
-    createCanvas(windowWidth, 800);
-    background(bgColor);
+    createCanvas(windowWidth - 50, 600);
+    background(color1.r, color1.g, color1.g );
 }
 
 function draw() {
@@ -12,8 +29,4 @@ function draw() {
 
 
 
-
-
-
-    
 }
