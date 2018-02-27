a1044c3e3d8510d3251cff4ca1ce2642e44bceb7
Mark Lannen <31551434+MarkLannenUM@users.noreply.github.com>
2018-02-27 14:44:44 -0700

    Problems sorted

--- hw-6 copy/sketch.js
+++ hw-6 copy/sketch.js
@@ -7,34 +7,33 @@ bub1.h = 25;
 
 var bub2 = {};
 bub2.x1 = 50;
-bub2.y1 = constrain(height * 0.5, height);
 bub2.w = 25;
 bub2.h = 25;
 
+
 //color variables
-var rfill = 100;
-var gfill = 100;
-var bfill = 100;
-var afill = 0.5;
+var rfill = 0;
+var gfill = 255;
+var bfill = 0;
+var afill = 255;
 
 // stroke variable
-var strW = 3;
+var strW = 1;
 
 
 function setup() {
 createCanvas(windowWidth, 600);
-background('rgb(158, 209, 251)');
+background(158, 209, 251);
 
-
+bub2.y1 = constrain(height * 0.5, height);
 }
 
 function draw() {
-
 // draw bubble 1
+stroke(rfill, gfill - 100, bfill, afill);
 strokeWeight(strW);
-stroke( 'rgba(rfill, gfill, bfill, afill)');
 // stroke('red');
-fill ( 'rgba(rfill - 50, gfill, bfill + 50, afill)');
+fill (rfill - 50, gfill, bfill + 50, afill);
 ellipse( bub1.x1, bub1.y1, bub1.w, bub1.h);
 
 // draw bubble 2
@@ -48,25 +47,25 @@ bub1.h = random(2, 5);
 
 // generate random bubbles - bub2
 bub2.x1 = random(0, width);
-bub2.y1 = random();
+bub2.y1 = random(height/2, height);
 bub2.w = random(2, 5);
 bub2.h = random(2, 5);
 
 // generate different stroke colors
-rfill = rfill % 3;
-gfill = gfill % 6;
-bfill = bfill % 9;
+// rfill = (rfill % 3) + 50;
+// gfill = gfill % 6;
+// bfill = bfill % 9;
 
 // generates different fill colors
-rfill = rfill % 3;
-gfill = gfill % 6;
-bfill = bfill % 9;
-
-
-
-strW = constrain(2, 1, 5);
-
-
+// rfill = rfill % 3;
+// gfill = gfill % 6;
+// bfill = bfill % 9;
+//
+//
+//
+// strW = constrain(2, 1, 5);
+//
+//
 
 
 
