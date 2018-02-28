55175e1987f5169e3db92044f06a1109ffa8e183
Mark Lannen <mark1.lannen@umontana.edu>
2018-02-27 20:07:27 -0700

    updated sketch

--- hw-6 copy/sketch.js
+++ hw-6 copy/sketch.js
@@ -7,15 +7,24 @@ bub1.h = 25;
 
 var bub2 = {};
 bub2.x1 = 50;
+bub2.y1 = (50);
 bub2.w = 25;
 bub2.h = 25;
 
 
-//color variables
-var rfill = 0;
-var gfill = 255;
+//fill color variables
+var rfill = 255;
+var gfill = 0;
 var bfill = 0;
 var afill = 255;
+
+rfill = rfill + 100;
+
+//stroke color variables
+var rst = 5;
+var gst = 200;
+var bst = 10;
+var ast = 150;
 
 // stroke variable
 var strW = 1;
@@ -25,15 +34,15 @@ function setup() {
 createCanvas(windowWidth, 600);
 background(158, 209, 251);
 
-bub2.y1 = constrain(height * 0.5, height);
+bub2.y1 = (height * 0.5, height);
 }
 
 function draw() {
 // draw bubble 1
-stroke(rfill, gfill - 100, bfill, afill);
+stroke(rst, gst, bst, ast);
 strokeWeight(strW);
 // stroke('red');
-fill (rfill - 50, gfill, bfill + 50, afill);
+fill (rfill, gfill, bfill, afill);
 ellipse( bub1.x1, bub1.y1, bub1.w, bub1.h);
 
 // draw bubble 2
@@ -42,30 +51,15 @@ ellipse( bub2.x1, bub2.y1, bub2.w, bub2.h);
 // generate random bubbles - bub1
 bub1.x1 = random(0, width);
 bub1.y1 = random(0, height * 0.5);
-bub1.w = random(2, 5);
-bub1.h = random(2, 5);
+bub1.w = random(2, 50);
+bub1.h = random(2, 50);
 
 // generate random bubbles - bub2
 bub2.x1 = random(0, width);
 bub2.y1 = random(height/2, height);
-bub2.w = random(2, 5);
-bub2.h = random(2, 5);
+bub2.w = random(2, 10);
+bub2.h = random(2, 10);
 
-// generate different stroke colors
-// rfill = (rfill % 3) + 50;
-// gfill = gfill % 6;
-// bfill = bfill % 9;
-
-// generates different fill colors
-// rfill = rfill % 3;
-// gfill = gfill % 6;
-// bfill = bfill % 9;
-//
-//
-//
-// strW = constrain(2, 1, 5);
-//
-//
 
 
 
