947e8adca25a0402cf4ae86955638710b4ff756e
Mark Lannen <mark1.lannen@umontana.edu>
2018-02-27 21:03:31 -0700

    updated sketch
    Moved elements into different quarters

--- hw-6 copy/sketch.js
+++ hw-6 copy/sketch.js
@@ -1,15 +1,34 @@
 //global variables
+
+// bubble 1
 var bub1 = {};
 bub1.x1 = 50;
 bub1.y1 = 50;
 bub1.w = 25;
 bub1.h = 25;
 
+// bubble 2
 var bub2 = {};
 bub2.x1 = 50;
-bub2.y1 = (50);
+bub2.y1 = 50;
 bub2.w = 25;
 bub2.h = 25;
+
+// rect 1
+var rect1 = {
+    x1: 50,
+    y1: 50,
+    w: 50,
+    h: 50
+};
+
+// rect 2
+var rect2 = {
+    x1: 50,
+    y1: 50,
+    w: 50,
+    h: 50
+};
 
 
 //fill color variables
@@ -60,6 +79,19 @@ strokeWeight(strW);
 fill (rfill, gfill, bfill, afill);
 ellipse( bub2.x1, bub2.y1, bub2.w, bub2.h);
 
+// draw rect1
+stroke(rst, gst, bst, ast);
+strokeWeight(strW);
+fill (rfill, gfill, bfill, afill);
+rect (rect1.x1, rect1.y1, rect1.w, rect1.h);
+
+// draw rect2
+stroke(rst, gst, bst, ast);
+strokeWeight(strW);
+fill (rfill, gfill, bfill, afill);
+rect (rect2.x1, rect2.y1, rect2.w, rect2.h);
+
+
 // generate random bubbles - bub1
 bub1.x1 = random(0, width * 0.5);
 bub1.y1 = random(0, height * 0.5);
@@ -68,10 +100,21 @@ bub1.h = random(2, 50);
 
 // generate random bubbles - bub2
 bub2.x1 = random(width * 0.5, width);
-bub2.y1 = random(height/2, height);
+bub2.y1 = random(0, height/2);
 bub2.w = random(2, bub2.w * 1.1 - 10);
 bub2.h = random(2, 15);
 
+// generate random rectangles for rect1
+rect1.x1 = random(0, width * 0.5);
+rect1.y1 = random( height * 0.5, height);
+rect1.w = random(2, 20);
+rect1.h = random (2, 20);
+
+// generate random rectangles for rect2
+rect2.x1 = random(width * 0.5, width);
+rect2.y1 = random(height * 0.5, height);
+rect2.w = random(2, 50);
+rect2.h = random (2, 50);
 
 
 
