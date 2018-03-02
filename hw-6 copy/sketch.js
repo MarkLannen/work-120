529e36f7bf50423898aa461d66ea2fa4d8ff7bc7
MarkLannenUM <GitErDone_565>
2018-03-01 18:01:59 -0700

    updated readme file

---
+++ hw-6 copy/sketch.js
@@ -0,0 +1,152 @@
+//global variables
+
+// bubble 1
+var bub1 = {};
+bub1.x1 = 50;
+bub1.y1 = 50;
+bub1.w = 25;
+bub1.h = 25;
+
+// bubble 2
+var bub2 = {};
+bub2.x1 = 50;
+bub2.y1 = 50;
+bub2.w = 25;
+bub2.h = 25;
+
+// rect 1
+var rect1 = {
+    x1: 50,
+    y1: 50,
+    w: 50,
+    h: 50
+};
+
+// rect 2
+var rect2 = {
+    x1: 50,
+    y1: 50,
+    w: 50,
+    h: 50
+};
+
+// triangle variables
+var tri1 = {
+    x1: 50,
+    y1: 50,
+    x2: 55,
+    y2: 55,
+    x3: 25,
+    y3: 50
+};
+
+// triangle position
+var tri_pos;
+
+//fill color variables
+var rfill = 255;
+var gfill = 0;
+var bfill = 0;
+var afill = 100;
+
+rfill = rfill + 100;
+
+//stroke color variables
+var rst = 10;
+var gst = 200;
+var bst = 200;
+var ast = 200;
+
+// stroke variable
+var strW = 1;
+
+
+function setup() {
+  createCanvas(windowWidth, 600);
+  background(141, 221, 255);
+}
+
+function draw() {
+// draw bubble 1
+stroke(rst, gst, bst, ast);
+strokeWeight(strW);
+fill (rfill, gfill, bfill, afill);
+ellipse( bub1.x1, bub1.y1, bub1.w, bub1.h);
+
+// draw bubble 2
+stroke(rst, gst, bst, ast);
+strokeWeight(strW);
+fill (rfill, gfill, bfill, afill);
+ellipse( bub2.x1, bub2.y1, bub2.w, bub2.h);
+
+// draw rect1
+stroke(rst, gst, bst, ast);
+strokeWeight(strW);
+fill (rfill, gfill, bfill, afill);
+rect (rect1.x1, rect1.y1, rect1.w, rect1.h);
+
+// draw rect2
+stroke(rst, gst, bst, ast);
+strokeWeight(strW);
+fill (rfill, gfill, bfill, afill);
+rect (rect2.x1, rect2.y1, rect2.w, rect2.h);
+
+// draw moving triangle
+stroke(rst, gst, bst, ast);
+strokeWeight(strW);
+fill (rfill, gfill, bfill, afill * 0.9);
+triangle (tri1.x1, tri1.y1, tri1.x2, tri1.y2, tri1.x3, tri1.y3);
+
+// move triangle back and forth across the screen
+tri_pos = (tri1.x1, tri1.y1, tri1.x2, tri1.y2, tri1.x3, tri1.y3);
+
+//animate triangle
+tri1.x1 = width * 0.49;
+tri1.y1 = (tri1.y1 + 48) % height;
+tri1.x2 = width * 0.49;
+tri1.y2 = (tri1.y2 + 49) % height;
+tri1.x3 = width * 0.51;
+tri1.y3 = (tri1.y3 + 51) % height;
+
+
+// change fill and stroke colors
+rfill = random(0, 255);
+rfill = constrain(rfill * 10, 0, 255);
+gfill = random(0, 255);
+gfill = constrain(gfill * 2, 0, 255);
+bfill = random(0, 255);
+bfill = constrain(bfill/2, 0, 255);
+afill = random(0, 255);
+strW = random(strW/2, 1);
+
+//alter stroke strokeWeight
+strW = random(sqrt(1), sqrt(4)/2);
+
+
+// generate random bubbles - bub1
+bub1.x1 = random(0, (width * 0.5) - 50);
+bub1.y1 = random(0, (height * 0.5) - 50);
+bub1.w = map(bub1.x1, 0, width/2, 50, 7);
+bub1.h = map(bub1.y1, 0, width/1.8, 35, 1);
+
+// generate random bubbles - bub2
+bub2.x1 = random((width * 0.5) + 50, width);
+bub2.y1 = random(0, (height/2) - 50);
+bub2.w = random(2, bub2.w * 1.1 - 10);
+bub2.h = random(2, 15);
+
+// generate random rectangles for rect1
+rect1.x1 = random(0, (width * 0.5) - 50);
+rect1.y1 = random((height * 0.5) + 30, height);
+rect1.w = random(2, 20);
+rect1.h = random (2, 20);
+
+// generate random rectangles for rect2
+rect2.x1 = random((width * 0.5) + 50, width);
+rect2.y1 = random((height * 0.5) + 50, height);
+rect2.w = map(rect2.x1, width, width/2, 50, 2);
+rect2.h = map(rect2.y1, width, width/2.1, 60, 2);
+
+
+
+}
--- hw-6/readme.md
+++ hw-6/readme.md
@@ -6,7 +6,7 @@ I found this the most challenging project to date. The main issue for me was tha
 ## Process
 1. I first created 2 sets of ellipses, declared variables for all their attributes, including color. I then did the same thing for a smaller group of ellipses and 2 groups of rectangles. Lastly, I divided the canvas in quarters and filled each quarter with one set of shapes.
 2. I then set random animations to distribute ellipses of various sizes, shapes and colors around the screen using as many of the math operators and functions as possible.
-3. I added a single column of triangles to move up and down the open center of the sketch and used modulo to keep it within the bounds of the canvas.   
+3. I added a single column of triangles to move up and down the open center of the sketch and used modulo to keep it within the bounds of the canvas.
 4. Lastly, I used the map function to increase and decrease the size of the elements based on how near or far they were from the centre of the canvas.
 5. The map function turned out to be the most complicated part of the sketch and I had to consult with Stephanie and Dr. Musick to finally get it to work.  Below is the code:
 
@@ -17,9 +17,13 @@ bub1.w = map(bub1.x1, 0, width/2, 50, 7);
 bub1.h = map(bub1.y1, 0, width/1.8, 35, 1);`
 
 
-5. A second issue was understanding modulo and finding a way to incorporate it into my sketch. I finally decided to use it to vary the frame rate.
-
 ## issues
 The biggest issue I encountered, apart from making effective use of the more complicated math functions, was that I used some p5 functions in my global variables (declared before draw and setup) and could not understand why they weren't working. I took the issue to Quinn in the lab and he pointed out that draw or setup needed to be declared before the computer knew that it was p5 code. This meant that if those p5 functions were declared outside of draw or setup that the computer would not 'know' to look in the p5_lib folder, which is where it would get the code to make the p5 functions work.
 
 I posted what I had learned on the github issues board. I'm hoping that I explained it correctly.  :)
+
+[Live sketch link](https://marklannenum.github.io/work-120/hw-6/)
+
+## Below is a capture of the sketch after it has been running for several minutes.
+
+!(Capture.JPG)
