b6d0a1bee39e53ae43420604fbd821d8c4b70e38
Mark Lannen <mark1.lannen@umontana.edu>
2018-03-13 22:04:44 -0600

    added ellipses

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -12,18 +12,29 @@ function draw() {
     let r = 250;
     let g = 100;
     let b = 50;
-    let fColor = ( r, g, b );
+    let a = 0.5;
+    let fColor = ( 'rgba(r, g, b, a)' );
 
 
 
 // Create arcs
 // declare x and y variables and increment them
 // set radions to random values based on x and y
-    // if (x <= width * 0.5 && y <= height * 0.5) {
-        for (let x = 50; x <= width; x += 50) {
-            for (let y = 50; y <= height-50; y+=50) {
-                arc( x, y, 50, 50, radians(random(x, y)), radians(random(x, y)));
-            }
+    for (let x = 50; x <= width; x += 50) {
+        for (let y = 50; y <= height-50; y += 50) {
+            arc( x, y, 50, 50, radians(random(x, y)), radians(random(x, y)));
         }
-    // }
+    }
+
+
+
+// if ()
+
+    fill('rgba( 200, 50, 100, 0.2)');
+    for (let x = 50; x <= width; x += 50) {
+        for (let y = 50; y <= height-50; y += 50) {
+            ellipse( x, y, 50, 50);
+        }
+    }
+
 }
