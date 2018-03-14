e9d316eca930b5ffa55efa3b88a6bb162140eb50
Mark Lannen <mark1.lannen@umontana.edu>
2018-03-13 21:20:42 -0600

    tried to move arcs into quadrant only

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -5,13 +5,13 @@ function setup() {
 
 function draw() {
     background(150, 225, 150);
-    frameRate(5);
+    frameRate(1);
     console.log(width);
 
 // declare variable for individual fill colors r, g, b and declare fColor for all fill colors
-    let r = 200;
+    let r = 250;
     let g = 100;
-    let b - 50;
+    let b = 50;
     let fColor = ( r, g, b );
 
 
@@ -19,10 +19,11 @@ function draw() {
 // Create arcs
 // declare x and y variables and increment them
 // set radions to random values based on x and y
-    for (let x = 50; x <= width; x += 50) {
-     for (let y = 50; y <= height-50; y+=50) {
-         arc( x, y, 50, 50, radians(random(x, y)), radians(random(x, y)));
-
-     }
-   }
+    // if (x <= width * 0.5 && y <= height * 0.5) {
+        for (let x = 50; x <= width; x += 50) {
+            for (let y = 50; y <= height-50; y+=50) {
+                arc( x, y, 50, 50, radians(random(x, y)), radians(random(x, y)));
+            }
+        }
+    // }
 }
