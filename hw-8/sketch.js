53369adbc72762228d87b0b0238aa1fb508c157b
Mark Lannen <mark1.lannen@umontana.edu>
2018-03-13 20:47:48 -0600

    declared new variables for fill color

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -8,10 +8,20 @@ function draw() {
     frameRate(5);
     console.log(width);
 
+// declare variable for individual fill colors r, g, b and declare fColor for all fill colors
+    let r = 200;
+    let g = 100;
+    let b - 50;
+    let fColor = ( r, g, b );
 
+
+
+// Create arcs
+// declare x and y variables and increment them
+// set radions to random values based on x and y
     for (let x = 50; x <= width; x += 50) {
      for (let y = 50; y <= height-50; y+=50) {
-         arc( x, y, 50, 50, radians(random(50, 250)), radians(random(0, 238)));
+         arc( x, y, 50, 50, radians(random(x, y)), radians(random(x, y)));
 
      }
    }
