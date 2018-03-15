9b5bf0ae32906af186a34909bbca18a5db03aa00
Mark Lannen <mark1.lannen@umontana.edu>
2018-03-15 13:58:26 -0600

    updated sketch

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -1,5 +1,13 @@
+
+let r = 250;
+let g = 100;
+let b = 50;
+let a = 0.5;
+
 function setup() {
     createCanvas( windowWidth, 600 );
+
+
 
 }
 
@@ -9,11 +17,8 @@ function draw() {
     console.log(width);
 
 // declare variable for individual fill colors r, g, b and declare fColor for all fill colors
-    let r = 250;
-    let g = 100;
-    let b = 50;
-    let a = 0.5;
-    let fColor = ( 'rgba(r, g, b, a)' );
+
+    let fColor = ( 'rgba(r, g, b, a)'  );
 
 
 
@@ -25,11 +30,13 @@ function draw() {
             arc( x, y, 50, 50, radians(random(x, y)), radians(random(x, y)));
         }
     }
-// if ()
+
 
 // Create ellipses that fill the top lef quarter of the screen
-    fill('rgba( 200, 50, 100, 0.2)');
+    fill( r, g, b, a);
+    // fill(fColor);
     stroke( 'rgba( 200, 200, 200, 0.5)')
+
     for (let x = 50; x <= width/2; x += 50) {
         for (let y = 50; y <= height/2; y += 50) {
             ellipse( x, y, 50, 50);
@@ -37,11 +44,15 @@ function draw() {
     }
 
 // Create rectangles that fill top right corner of screen
-    fill('rgba( 100, 50, 200, 0.2)');
+    fill('rgba( r, g, b, a)');
     for (let x = width/2 + 15; x <= width; x += 50 ) {
       for (let y = height/2 + 24; y <= height; y += 50) {
         rect( x, y, 50, 50);
       }
+    }
+
+    if ( mouseX <= width/2) {
+        fill('rgba (200, 50, 100, 1)');
     }
 
 
