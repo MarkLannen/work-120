6f7dc96f4951c842fec3adfc027c0a0d6d14bc89
MarkLannenUM <GitErDone_565>
2018-03-15 17:06:26 -0600

    took out variables for color and added absolute values

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -1,14 +1,11 @@
 
-let r = 250;
-let g = 100;
-let b = 50;
-let a = 0.5;
+// let r = 250;
+// let g = 100;
+// let b = 50;
+// let a = 0.5;
 
 function setup() {
     createCanvas( windowWidth, 600 );
-
-
-
 }
 
 function draw() {
@@ -16,15 +13,11 @@ function draw() {
     frameRate(1);
     console.log(width);
 
-// declare variable for individual fill colors r, g, b and declare fColor for all fill colors
-
-    let fColor = ( 'rgba(r, g, b, a)'  );
-
-
-
 // Create arcs
 // declare x and y variables and increment them
 // set radions to random values based on x and y
+fill('rgba(250, 100, 50, 0.5)');
+stroke( 200, 200, 200 )
     for (let x = 50; x <= width; x += 50) {
         for (let y = 50; y <= height-50; y += 50) {
             arc( x, y, 50, 50, radians(random(x, y)), radians(random(x, y)));
@@ -33,10 +26,6 @@ function draw() {
 
 
 // Create ellipses that fill the top lef quarter of the screen
-    fill( r, g, b, a);
-    // fill(fColor);
-    stroke( 'rgba( 200, 200, 200, 0.5)')
-
     for (let x = 50; x <= width/2; x += 50) {
         for (let y = 50; y <= height/2; y += 50) {
             ellipse( x, y, 50, 50);
@@ -44,21 +33,15 @@ function draw() {
     }
 
 // Create rectangles that fill top right corner of screen
-    fill('rgba( r, g, b, a)');
     for (let x = width/2 + 15; x <= width; x += 50 ) {
       for (let y = height/2 + 24; y <= height; y += 50) {
         rect( x, y, 50, 50);
       }
     }
 
-    if ( mouseX <= width/2) {
-        fill('rgba (200, 50, 100, 1)');
-    }
-
-
-
-
-
+    // if ( mouseX <= width/2) {
+    //     fill('rgba (200, 50, 100, 1)');
+    // }
 
 
 
