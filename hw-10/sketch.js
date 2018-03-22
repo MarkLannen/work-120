ae3e42ab689a2b8bfd6ea8623d66da13562429db
MarkLannenUM <GitErDone_565>
2018-03-22 16:49:41 -0600

    set up new sketch

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,1 +1,10 @@
 /*jshint esversion: 6 */
+function setup() {
+  createCanvas(windowWidth, 600);
+  background(200, 100, 150);
+}
+
+function draw() {
+  stroke(100, 200, 250);
+  line(5, 10, 15, 20);
+}
