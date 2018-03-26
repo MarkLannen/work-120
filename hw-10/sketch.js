2cf29e0131361f0013b8287e1066401fd82e2ad5
Mark Lannen <mark1.lannen@umontana.edu>
2018-03-26 16:11:45 -0600

    sketch10 changes

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,24 +1,31 @@
 /*jshint esversion: 6 */
+let x1 = 0;
+let y1 = 0;
+
 function setup() {
     createCanvas( windowWidth, 600 );
-    // define array for line (growth) parameters
-    let growth = [x1, y1, x2, y2];
-
-    // comments
-
-
-
+    background(150, 225, 150);
+    for ( let i = 0; i < 10; i++) {
+    }
 }
 
 function draw() {
-    background(150, 225, 150);
-
-    // function that creates growing plant
-    function growingPlant() {
-    line(x1, y1, x2, y2);
-
-    growingPlant();
+    for ( let i = 0; i < 10; i++) {
+        //
+        frameRate(1);
+        drawLine();
+        moveLine();
 
     }
+}
 
+function drawLine() {
+    strokeWeight(5);
+    stroke(300);
+    line(x1, y1, x1 + random(50), y1 + random(50));
+}
+
+function moveLine() {
+    x1 = x1 + random(2);
+    y1 = y1 + random(2);
 }
