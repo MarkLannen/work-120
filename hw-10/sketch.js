5eaad074d0bf7081e8431051212d1e8a373e09eb
Mark Lannen <mark1.lannen@umontana.edu>
2018-03-27 17:36:08 -0600

    Insert comments on all code

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,21 +1,26 @@
 /*jshint esversion: 6 */
+// set both global variables
 let x1 = 0;
 let y1 = 0;
 
 
-
+// create canvas
+//set background color
+//set up first array to iterate the number of lines
 function setup() {
     createCanvas( windowWidth, 600 );
     background(150, 225, 150);
-    for ( let i = 0; i < 10; i++) {
-    }
+    // for ( let i = 0; i < 10; i++) {
+    // }
 }
 
 function draw() {
-
+    //second array to iterate number of lines generated (10)
+    //set frameRate
+    //call both functions
     for ( let i = 0; i < 10; i++) {
         //
-        frameRate(5);
+        frameRate(1);
         drawLine();
         moveLine();
 
@@ -27,7 +32,7 @@ function draw() {
         if (y1 > 600 || y1 < 0) {
             y1 = y1 * -1;
         }
-
+        // set constraints on both variables so that they stay within the screen
         x1 = constrain(x1, 0, windowWidth);
         y1 = constrain(y1, 0, 600);
 
@@ -35,13 +40,13 @@ function draw() {
 
 }
 
-
+// define drawLine function
 function drawLine() {
     strokeWeight(random(5));
     stroke(random(255), random(255), random(255));
     line(x1, y1, x1 + random(50), y1 + random(50));
 }
-
+// define moveLine function
 function moveLine() {
     x1 = x1 + random(5);
     y1 = y1 + random(15);
