9891de5ddd22c1da23d5a90e0d3f501f97b23c56
Mark Lannen <mark1.lannen@umontana.edu>
2018-03-27 17:24:34 -0600

    commit changes

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -2,6 +2,8 @@
 let x1 = 0;
 let y1 = 0;
 
+
+
 function setup() {
     createCanvas( windowWidth, 600 );
     background(150, 225, 150);
@@ -10,26 +12,37 @@ function setup() {
 }
 
 function draw() {
+
     for ( let i = 0; i < 10; i++) {
         //
-        frameRate(1);
+        frameRate(5);
         drawLine();
         moveLine();
 
+        // bounce lines back across screen
+        if (x1 > windowWidth || x1 < 0) {
+            x1 = x1 * -1;
+        }
+
+        if (y1 > 600 || y1 < 0) {
+            y1 = y1 * -1;
+        }
+
+        x1 = constrain(x1, 0, windowWidth);
+        y1 = constrain(y1, 0, 600);
+
     }
+
 }
+
 
 function drawLine() {
     strokeWeight(5);
-    stroke(300);
+    stroke(random(255), random(255), random(255));
     line(x1, y1, x1 + random(50), y1 + random(50));
 }
 
 function moveLine() {
-    x1 = x1 + random(2);
-    y1 = y1 + random(2);
-}
-
-function empytFunc() {
-
+    x1 = x1 + random(5);
+    y1 = y1 + random(15);
 }
