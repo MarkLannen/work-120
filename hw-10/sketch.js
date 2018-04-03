ecbf7d372063d4c40312a80df32405554c763f73
Mark Lannen <mark1.lannen@umontana.edu>
2018-04-02 18:33:14 -0600

    updated js sketch

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 // set both global variables
-let x1 = 0;
-let y1 = 0;
+let lines = [];
+
 
 
 // create canvas
@@ -9,45 +9,61 @@ let y1 = 0;
 //set up first array to iterate the number of lines
 function setup() {
     createCanvas( windowWidth, 600 );
+    frameRate(30);
+
     background(150, 225, 150);
     // for ( let i = 0; i < 10; i++) {
     // }
+
+    for (var i = 0; i < 3; i++) {
+        lines.push(
+            {
+                x: 0,
+                y: 0,
+                dx: 1,
+                dy: 1,
+                color: color(random(255), random(255), random(255))
+            }
+        );
+    }
 }
 
 function draw() {
     //second array to iterate number of lines generated (10)
     //set frameRate
     //call both functions
-    for ( let i = 0; i < 10; i++) {
+    for ( let i = 0; i < lines.length; i++) {
         //
-        frameRate(1);
-        drawLine();
-        moveLine();
-
-        // bounce lines back across screen
-        if (x1 > windowWidth || x1 < 0) {
-            x1 = x1 * -1;
-        }
-
-        if (y1 > 600 || y1 < 0) {
-            y1 = y1 * -1;
-        }
-        // set constraints on both variables so that they stay within the screen
-        x1 = constrain(x1, 0, windowWidth);
-        y1 = constrain(y1, 0, 600);
-
+        drawLine(i);
     }
 
 }
 
 // define drawLine function
-function drawLine() {
+function drawLine(idx) {
+    let temp_x, temp_y;
+
+    temp_x = lines[idx].x + (random(50) * lines[idx].dx);
+    temp_y = lines[idx].y + (random(50) * lines[idx].dy);
+
+
     strokeWeight(random(5));
-    stroke(random(255), random(255), random(255));
-    line(x1, y1, x1 + random(50), y1 + random(50));
-}
-// define moveLine function
-function moveLine() {
-    x1 = x1 + random(5);
-    y1 = y1 + random(15);
+    stroke( lines[idx].color );
+    line(lines[idx].x, lines[idx].y, temp_x, temp_y);
+
+    lines[idx].x = temp_x;
+    lines[idx].y  = temp_y;
+
+
+    // bounce lines back across screen
+    if (lines[idx].x > windowWidth || lines[idx].x < 0) {
+        lines[idx].dx = lines[idx].dx * -1;
+    }
+
+    if (lines[idx].y > 600 || lines[idx].y < 0) {
+        lines[idx].dy = lines[idx].dy * -1;
+    }
+    // set constraints on both variables so that they stay within the screen
+    lines[idx].x = constrain(lines[idx].x, 0, windowWidth);
+    lines[idx].y = constrain(lines[idx].y, 0, 600);
 }
