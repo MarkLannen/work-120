15ff7c4d1caaf28fe0da43465b1619704f013228
Mark Lannen <mark1.lannen@umontana.edu>
2018-04-07 17:30:26 -0600

    updated sketch

--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -10,25 +10,49 @@ function setup() {
     createCanvas( windowWidth, 600 );
     frameRate(30);
     background(221, 244, 251);
+    // for loop to add 25 objects to Lines array
     for (let i = 0; i < num_of_lines; i++) {
     lines.push( new Lines() );
-}
+    }
 
 }
-
+// draw function
 function draw() {
 
-
+    // for loop to add # of lines in Lines array to method growLines
+    for (var i = 0; i < lines.length; i ++) {
+        lines[i].growLines;
+    }
 }
 
+/////////////////////////////////////////
+//      Lines class
+/////////////////////////////////////////
 class Lines {
     constructor(x, y, dx, dy) {
         this.x = 100;
         this.y = 100;
         this.dx = 150;
         this.dy = 150;
+        this.color(random(255, 10));
     }
+}
 
+// call this method once per frame to start and then generate new lines.
+growLines() {
+    this.startLine();
+    this.moveLine();
+}
 
+// method to start lines
+startline() {
+    stroke(this.color);
+    line(x, y, dx, dy);
 
 }
+
+
+
+
+// method to move lines
+moveLine();
