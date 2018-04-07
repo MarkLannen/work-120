d67f6cf8d2481a09b12a1525b2b0fcd012918184
Mark Lannen <mark1.lannen@umontana.edu>
2018-04-07 16:58:57 -0600

    started new js sketch

---
+++ hw-11/sketch.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+// set global variable in array 'lines'
+let lines = [];
+let num_of_lines = 25;
+
+// create canvas
+//set background color
+
+function setup() {
+    createCanvas( windowWidth, 600 );
+    frameRate(30);
+    background(221, 244, 251);
+    let lines = new Lines[];
+
+}
+
+function draw() {
+
+
+}
+
+class Lines {
+    constructor(x, y, dx, dy) {
+        this.x = 100;
+        this.y = 100;
+        this.dx = 150;
+        this.dy = 150;
+    }
+
+
+
+}
