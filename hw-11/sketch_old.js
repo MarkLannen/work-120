a010a7d69d690d0e926b544705313e7916e5099e
Mark Lannen <mark1.lannen@umontana.edu>
2018-04-08 14:22:36 -0600

    started new sketch

--- hw-11/sketch_old.js
+++ hw-11/sketch_old.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 // set global variable in array 'lines'
 let lines = [];
-
+let num_of_lines = 25;
 
 // create canvas
 //set background color
@@ -10,13 +10,19 @@ function setup() {
     createCanvas( windowWidth, 600 );
     frameRate(30);
     background(221, 244, 251);
-    lines = new Lines();
+    // for loop to add 25 objects to Lines array
+    for (let i = 0; i < num_of_lines; i++) {
+    lines.push( new Lines() );
+    }
 
 }
 // draw function
 function draw() {
 
-
+    // for loop to add # of lines in Lines array to method growLines
+    for (var i = 0; i < lines.length; i ++) {
+        lines[i].growLines;
+    }
 }
 
 /////////////////////////////////////////
@@ -37,7 +43,22 @@ class Lines {
 
 
 // call this method once per frame to start and then generate new lines.
+    growLines() {
+    this.startLine();
+    this.moveLine1();
+    this.moveLine2();
+    }
 
+// method to start lines
+    startline() {
+    stroke(this.color);
+    line(x, y, dx, dy);
+    }
+
+// method to create first new line segment
+    moveLine1() {
+    this.x += this.x;
+    }
 }
 
 // method to create second new line segment
