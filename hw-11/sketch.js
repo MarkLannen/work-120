493f50d96f30fa15805627893720f26716b235f7
Mark Lannen <mark1.lannen@umontana.edu>
2018-04-08 17:01:19 -0600

    fixed move function

--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -15,8 +15,7 @@ function setup() {
 function draw() {
 
     lines.createLines();
-    // lines.moveLines1();
-
+    lines.moveLines1();
 }
 
 /////////////////////////////////////////
@@ -26,26 +25,25 @@ class Lines {
     constructor() {
         this.x = 25;
         this.y = 25;
-        this.end_x = this.x += 100;
-        this.end_y = this.y += 100;
+        this.end_x = 100;
+        this.end_y = 100;
         // this.color(random(255, 10));
-        // this.move_dx = this.dx;
-        // this.move_dy = this.dy;
     }
-
-    // method for moving lines
-    // moveLines1() {
-    //     stroke(200);
-    //     strokeWeight(3);
-    //     this.x = this.end_x;
-    //     this.y = this.end_y;
-    // }
 
     // method for creating first lines
     createLines() {
         stroke(200);
         strokeWeight(8);
         line(this.x, this.y, this.end_x, this.end_y);
+    }
+    // method for moving lines
+    moveLines1() {
+        stroke(200);
+        strokeWeight(3);
+        this.x = this.end_x;
+        this.y = this.end_y;
+        this.end_x = this.end_x += 100;
+        this.end_y = this.end_x += 100;
     }
 
 
