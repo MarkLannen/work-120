cd86987af0226ce3ec0f7a0f4d13227a7b5b4a8d
Mark Lannen <mark1.lannen@umontana.edu>
2018-04-15 12:04:06 -0600

    started hw 12 project and added classes to new js files

---
+++ hw-12/sketch.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+// set global variable in array 'tails'
+let tails = [];
+
+// create canvas
+// set background color
+function setup() {
+    createCanvas( windowWidth, 600 );
+    frameRate(10);
+    background(221, 244, 251);
+    // for loop that adds objects to array 'Lines'
+    for (let i = 0; i < 10; i ++) {
+    tails[i] = new Tails (0, 0, 50, 50, (color(random(255), random(255), random(255))));
+    }
+
+}
+// draw function
+function draw() {
+    background(221, 244, 251, 90);
+    // for loop to enacts all methods each draw loop
+    // and create specified # of new tails each loop.
+    for (let i = 0; i < tails.length; i ++) {
+        // calls method to create tail
+        tails[i].createLines();
+        // calls method to move x and y coordinates of new tail to end of previous tail
+        tails[i].moveLines();
+        // calls method to redirect tails that go off-screen
+        tails[i].edgeLines();
+        // calls method that constrains tails to stay within specified window width and height
+        tails[i].constrainLines();
+    }
+}
--- hw-12/tails.js
+++ hw-12/tails.js
@@ -1,40 +1,4 @@
-/*jshint esversion: 6 */
-// set global variable in array 'lines'
-let lines = [];
-
-// create canvas
-// set background color
-function setup() {
-    createCanvas( windowWidth, 600 );
-    frameRate(40);
-    background(221, 244, 251);
-    // for loop that adds objects to array 'Lines'
-    for (let i = 0; i < 10; i ++) {
-    lines[i] = new Lines (0, 0, 50, 50, (color(random(255), random(255), random(255))));
-    }
-
-}
-// draw function
-function draw() {
-    background(221, 244, 251, 90)
-    // for loop to enacts all methods each draw loop
-    // and create specified # of new lines each loop.
-    for (let i = 0; i < lines.length; i ++) {
-        // calls method to create line
-        lines[i].createLines();
-        // calls method to move x and y coordinates of new line to end of previous line
-        lines[i].moveLines();
-        // calls method to redirect lines that go off-screen
-        lines[i].edgeLines();
-        // calls method that constrains lines to stay within specified window width and height
-        lines[i].constrainLines();
-    }
-}
-
-/////////////////////////////////////////
-//      Lines class
-/////////////////////////////////////////
-class Lines {
+class Tails {
     constructor(temp_x, temp_y, temp_end_x, temp_end_y, temp_color) {
         this.x = temp_x + (random(1200));
         this.y = temp_y + (random(600));
@@ -44,14 +8,14 @@ class Lines {
         // this.color(random(255, 10));
     }
 
-    // method for creating first lines
+    // method for creating first tails
     createLines() {
         stroke(this.color);
         strokeWeight(random(1,8));
         // line(this.x, this.y, this.end_x += random(-200, 50), this.end_y += random(-200, 50));
         line(this.x, this.y, this.end_x, this.end_y);
     }
-    // method for moving lines
+    // method for moving tails
     moveLines() {
         stroke(this.color);
         strokeWeight(3);
