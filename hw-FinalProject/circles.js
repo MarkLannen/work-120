e03182d755add3fff5ad02234f5c1c95dda73644
Mark Lannen <mark1.lannen@umontana.edu>
2018-04-26 21:16:16 -0600

    started sketch

--- hw-FinalProject/circles.js
+++ hw-FinalProject/circles.js
@@ -1,1 +1,18 @@
 /*jshint esversion: 6 */
+
+// create circles class
+class Circles {
+    constructor(c_tempX, c_tempY, c_tempR, c_tempColor) {
+        this.x = c_tempX;
+        this.y = c_tempY;
+        this.r = c_tempR;
+        this.x_Delta = random([-1, 1]);
+        this.y_Delta = random([-1, 1]);
+        this.r_Delta = random([-1, 1]);
+        this.c_Color = c_tempColor;
+        
+
+
+    }
+
+}
