8edb25f108e288b2fc8db290281f1f7cdc9ed985
Mark Lannen <mark1.lannen@umontana.edu>
2018-04-29 10:31:30 -0600

    updated circle order

--- hw-FinalProject/sketch.js
+++ hw-FinalProject/sketch.js
@@ -20,9 +20,14 @@ function setup() {
     background(bgimg);
 
     // create circle objects
-    circle1 = new Circle (239, 369, 60, (color(47, 35, 94)));
-    circle2 = new Circle (239, 369, 140, (color(180, 173, 131)));
-    circle3 = new Circle (700, 500, 250, (color(50, 200, 100)));
+    circle1 = new Circle (239, 369, 314, (color(23, 22, 22)));
+    circle2 = new Circle (239, 369, 280, (color(180, 173, 131)));
+    circle3 = new Circle (239, 369, 120, (color(47, 35, 94)));
+
+
+    // circle1 = new Circle (239, 369, 120, (color(47, 35, 94)));
+    // circle2 = new Circle (239, 369, 280, (color(180, 173, 131)));
+    // circle3 = new Circle (239, 369, 314, (color(23, 22, 22)));
 
 
 
